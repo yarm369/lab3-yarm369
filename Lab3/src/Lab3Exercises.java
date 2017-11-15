0866ae7d4b26d6457f2bae44bb06804a5232b6cc
4015c15 <4015c15@limon.ece.uprm.edu>
2017-11-15 16:35:47 -0400

    The name of the window has been changed.It has a new size of 20x20 located in the center of the screen. Also it has been fixed the close on exit.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,9 +4,8 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Yacenia A. Rios");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setSize(200, 200);
 		myFrame.setLocationRelativeTo(null);
-		
+		myFrame.setSize(200, 200);		
 		myFrame.setVisible(true);
 	}
 }
